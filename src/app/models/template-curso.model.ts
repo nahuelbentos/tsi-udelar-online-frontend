8e3e997c47fd4aa8ecc8e4e8ce4532bc5a49138e
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-10-31 16:15:15 -0300

    Fin de ABM curso - v1

--- src/app/models/template-curso.model.ts
+++ src/app/models/template-curso.model.ts
@@ -1,7 +1,10 @@
+import { Seccion } from './seccion.model';
+
 export class TemplateCurso {
   constructor(
     public nombre: string,
     public descripcion?: string,
-    public templateCursoId?: string
+    public templateCursoId?: string,
+    public secciones?: Seccion[]
   ) {}
 }
