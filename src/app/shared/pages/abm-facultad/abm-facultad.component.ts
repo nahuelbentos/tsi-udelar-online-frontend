c229a6d131d3d49dc3dd381e6b87e4aa6eeea2df
Santiago Chabert <santiagochabert@gmail.com>
2020-11-22 17:03:20 -0300

    merge master

--- src/app/shared/pages/abm-facultad/abm-facultad.component.ts
+++ src/app/shared/pages/abm-facultad/abm-facultad.component.ts
@@ -95,6 +95,39 @@ export class AbmFacultadComponent implements OnInit {
     // en vez de administrador y queda pronto
     this.location.back();
   }
+  onUploadClicked(event) {
+    console.log('onUploadClicked:: ', event);
+  }
+
+  async onSelectedFilesChanged(fileList: FileList) {
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < fileList.length; i++) {
+      const file: File = fileList.item(i);
+      console.log(file);
+
+      this.archivoNombre = file.name.split('.')[0];
+      this.archivoExtension = file.name.split('.')[
+        file.name.split('.').length - 1
+      ];
+      console.log('arcchivoNombre:: ', file.name.split('.')[0]);
+      console.log(
+        'arcchivoExtension:: ',
+        file.name.split('.')[file.name.split('.').length - 1]
+      );
+
+      this.getBase64(file.slice());
+    }
+  }
+
+  getBase64(file) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    // reader.onload = () => (this.archivoData = reader.result.toString());
+    reader.onload = () => {
+      this.archivoData = reader.result.toString();
+    };
+    reader.onerror = (error) => console.log('Error: ', error);
+  }
 
   guardarFacultad(event: Event) {
     event.preventDefault();
